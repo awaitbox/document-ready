a6e067132f4ec12abfc1d5655254a9b3deb6ad9d
Joe Pea <joe@trusktr.io>
2018-01-05 13:48:04 -0800

    trim comment doc

--- src/index.js
+++ src/index.js
@@ -2,25 +2,8 @@
  * Await for this to run code after the DOM has been parsed and loaded (but not
  * sub-resources like images, scripts, etc).
  *
- * @example
- * ```js
- * async function main() {
- *     await documentReady()
- *     console.log('Document ready!')
- * }
- * main()
- * ```
- *
- * The _passThrough private arg is not public API, it makes it useful to
- * write Promise chains like this:
- *
- * @example
- * ```js
- * fetch( ... )
- *   .then( ... )
- *   .then( documentReady ) // passes data through
- *   .then( data => console.log( 'use the data!', data ) )
- * ```
+ * The _passThrough arg is not for public use, it's for making data pass
+ * through in promise chains.
  */
 export default
 async function documentReady( _passThrough ) {
